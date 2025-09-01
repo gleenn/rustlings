e219dfb1c8cbc00910774447a88c4e7719c34977
Glenn Jahnke <gleenn@gmail.com>
2025-08-31 23:12:13 -0600

    Update modules2.rs

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -6,21 +6,20 @@ mod delicious_snacks {
     // use self::fruits::PEAR as ???;
     // use self::veggies::CUCUMBER as ???;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
 }
 
+use delicious_snacks::fruits;
+use delicious_snacks::veggies;
+
 fn main() {
-    println!(
-        "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie,
-    );
+    println!("favorite snacks: {} and {}", fruits::PEAR, veggies::CARROT);
 }
