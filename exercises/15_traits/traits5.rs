62d52d6d5721674429bfa257271e224d716c9c7c
Glenn Jahnke <gleenn@gmail.com>
2025-09-12 03:12:55 -0700

    Update traits5.rs

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -19,13 +19,11 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+fn some_func(item: impl OtherTrait + SomeTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
-fn main() {
-    // You can optionally experiment here.
-}
+fn main() {}
 
 #[cfg(test)]
 mod tests {
